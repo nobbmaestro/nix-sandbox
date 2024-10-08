7689f647b8878a626554b7a05f3c5423b0376a76
Norbert Batiuk <norbertbatiuk@Norberts-Virtual-Machine.local>
2024-10-08 13:36:08 -0700

    Fix Kitty not being indexed by MacOS

--- flake.nix
+++ flake.nix
@@ -9,12 +9,13 @@
 
   outputs = inputs@{ self, nix-darwin, nixpkgs }:
   let
-    configuration = { pkgs, ... }: {
+    configuration = { pkgs, config, ... }: {
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
       environment.systemPackages =
         [
 		pkgs.kitty
+		pkgs.mkalias
 		pkgs.neovim
 		pkgs.tmux
 		pkgs.lazygit
@@ -23,6 +24,27 @@
       fonts.packages = [
       	(pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
       ];
+
+      system.activationScripts.applications.text = let
+      		env = pkgs.buildEnv {
+			name = "system-applications";
+			paths = config.environment.systemPackages;
+			pathsToLink = "/Applications";
+		};
+	in
+		pkgs.lib.mkForce ''
+		# Set up applications.
+		echo "setting up /Applications..." >&2
+		rm -rf /Applications/Nix\ Apps
+		mkdir -p /Applications/Nix\ Apps
+		find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+		while read src; do
+			app_name=$(basename "$src")
+			echo "copying $src" >&2
+			${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+		done
+		'';
+	
 
       # Auto upgrade nix package and the daemon service.
       services.nix-daemon.enable = true;
